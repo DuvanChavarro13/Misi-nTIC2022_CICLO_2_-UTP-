97b4d327f5c2575a27a51d12da6ca49afa86e07d
Duvan Felipe Chavarro Gutiérrez <70192135+DuvanChavarro13@users.noreply.github.com>
2021-07-07 17:42:48 -0500

    nuevo proyecto

---
+++ Unidad1/proyecto_clase02/src/main/java/co/edu/utp/misiontic2020/c2/Ejercicio1.java
@@ -0,0 +1,59 @@
+package co.edu.utp.misiontic2020.c2;
+
+import java.util.*;
+public class Ejercicio1 {
+
+    //Algoritmo: Número de la suerte (OBJETIVO)
+    //1) Solicitar al usuario la fecha en formato String
+    //2) Extraer los pedazos que corresponden al mes, dia y año. (split)
+    //3) Cada pedazo conventirlo en un valor numérico 
+    //4) Sumar los pedazos y almacenarlos
+    //5) De la sumatoria extraer las cifras
+    //6) Sumar las cifras y retornar el número de la suerte
+    //TRADUCIR A JAVA!! 
+
+    //Sección de funciones o métodos
+    public static int obtenerNumeroSuerte(String fecha){
+        int numeroSuerte = 0; 
+
+        //Extraer cada pedazo al String
+        String [] arregloFecha = fecha.split("/",5);
+
+        int valorDia = Integer.parseInt(arregloFecha[0]); 
+        int valorMes = Integer.parseInt(arregloFecha[1]);
+        int valorAño = Integer.parseInt(arregloFecha[2]);
+ 
+        int sumatoriaPartesFecha = valorDia + valorMes + valorAño;
+        //System.out.println("sumatoriaPartesFecha: " + sumatoriaPartesFecha);
+
+        //Ahora de la sumatoria extraer las cifras
+        String sumatoriaCadena = Integer.toString(sumatoriaPartesFecha);
+        for (int i = 0; i < sumatoriaCadena.length(); i++){
+            numeroSuerte = numeroSuerte + Character.getNumericValue(sumatoriaCadena.charAt(i));
+            //System.out.println("numero de la suerte: " + numeroSuerte);
+        }
+        return numeroSuerte;
+    }
+
+    //Interacción////////////
+    public static void saludo(){
+        System.out.println("App Número Suerte!!");
+    }
+
+    public static String leerFecha(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Ingresar la fecha DD/MM/AAAA: ");
+        return sc.nextLine();
+    }
+
+    public static void reportarNumeroSuerte(int numeroSuerte){
+        System.out.println("El número de la suerte es: " + numeroSuerte);
+    }
+
+    public static void main(String[] args) {
+        
+        saludo();
+        reportarNumeroSuerte(obtenerNumeroSuerte(leerFecha()));
+    }
+    
+}
