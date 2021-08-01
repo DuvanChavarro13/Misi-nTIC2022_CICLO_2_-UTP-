952f49df3f39d81cb4a118b227a328a51c6698bc
Duvan Felipe Chavarro Gutiérrez <70192135+DuvanChavarro13@users.noreply.github.com>
2021-08-01 15:31:07 -0500

    Ultima actualizacion de todas las carpetas hasta Unidad 5

--- Clase_Magistral_Unidad_1_y_2/sistema_de_notas/src/main/java/co/edu/utp/misiontic2022/c2/Materia.java
+++
@@ -1,9 +0,0 @@
-package co.edu.utp.misiontic2022.c2;
-
-public class Materia {
-
-    //Atributos  
-    public String nombre;
-    private double promedioAjustado; 
-    
-}
--- Unidad3/tres_en_raya/src/main/java/co/edu/utp/misiontic2022/c2/App.java
+++ Unidad3/tres_en_raya/src/main/java/co/edu/utp/misiontic2022/c2/App.java
@@ -1,13 +1,64 @@
 package co.edu.utp.misiontic2022.c2;
 
+import java.util.ArrayList;
+
 /**
- * Hello world!
+ * Tres en raya
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App{
+    public static void main( String[] args ){
+
+        System.out.println( "Bienvenido Tres en Raya" );
+
+        // //Construir tablero
+        // Tablero tablero = new Tablero();
+
+        // //Mostrar antes de movimiento
+        // tablero.mostrarTablero();
+
+        // //Construir jugadorX
+        // JugadorX jugadorX = new JugadorX("TripulanteX", 10, "X");
+        // JugadorO jugadorO = new JugadorO("TripulanteO", 1, "O");
+
+        //El jugadorX va a realizar un movimiento aleatorio
+        //jugadorX.ejecutarEstrategiaAleatoria(tablero);
+        
+        // //Solicitar al jugadorX varios movimientos de su estilo de juego
+        // jugadorX.ejecutarEstrategiaEspecifica(tablero);
+        // jugadorX.ejecutarEstrategiaAleatoria(tablero);
+        // jugadorX.ejecutarEstrategiaEspecifica(tablero);
+
+        //Solicitar jugadas o movimientos al jugadorO
+        // jugadorO.ejecutarEstrategiaEspecifica(tablero);
+        // jugadorO.ejecutarEstrategiaEspecifica(tablero);
+        // jugadorO.ejecutarEstrategiaEspecifica(tablero); 
+        
+        // //Solicitar movimientos alternados a la versión automática de los jugadores
+        // jugadorO.ejecutarEstrategiaEspecifica(tablero);
+        // jugadorX.ejecutarEstrategiaEspecifica(tablero);
+        // jugadorO.ejecutarEstrategiaEspecifica(tablero);
+        // jugadorX.ejecutarEstrategiaEspecifica(tablero);
+
+        // //Alternar turnos manuales
+        // jugadorO.realizarMovimientoManual(tablero);
+        // tablero.mostrarTablero();
+        // jugadorX.realizarMovimientoManual(tablero);
+        // tablero.mostrarTablero();
+        // jugadorO.realizarMovimientoManual(tablero);
+        // tablero.mostrarTablero();
+        // jugadorX.realizarMovimientoManual(tablero);
+        // tablero.mostrarTablero();
+
+        // //Mostrar tablero después del movimiento
+        // tablero.mostrarTablero();
+
+        //Construir el juego
+        Juego juegoTresEnRaya = new Juego("Edgar","Mónica");
+
+        //Iniciar la aplicación
+        //juegoTresEnRaya.ejecutarJuego(ParametroLogico.MODO_SIMULACION);
+        juegoTresEnRaya.ejecutarJuego(ParametroLogico.MODO_MANUAL);
+
     }
 }
